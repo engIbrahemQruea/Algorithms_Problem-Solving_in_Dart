f5673fb7d5d5fb0436f622728a08bfde9371d636
engIbrahemQruea <mmany6186@gmail.com>
2025-12-14 21:07:25 +0300

    problem_07 average of three mark

---
+++ level1/bin/problem_07.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:level1/level1.dart';
+
+void main() {
+  stdout.write('Enter Mark one : ');
+  String? inputOne = stdin.readLineSync();
+  stdout.write('Enter Mark tow : ');
+  String? inputTwo = stdin.readLineSync();
+  stdout.write('Enter Mark three : ');
+  String? inputThree = stdin.readLineSync();
+
+  int numberOne = stringToInt(inputOne!);
+  int numberTwo = stringToInt(inputTwo!);
+  int numberThree = stringToInt(inputThree!);
+
+int averageMark=(numberThree + numberTwo + numberOne) ~/ 3;
+  print('Average Mark  is $averageMark');
+}
