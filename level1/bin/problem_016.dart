4f09de667c3e595a500881f5142a4c9fdc9d0628
engIbrahemQruea <mmany6186@gmail.com>
2025-12-14 22:28:27 +0300

    Problem #16 Circle Area Inscribed in a Square

---
+++ level1/bin/problem_016.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:level1/level1.dart';
+
+void main() {
+  stdout.write('Enter Raids : ');
+  String? inputOne = stdin.readLineSync();
+
+
+  int raids = stringToInt(inputOne!);
+
+  double area =(3.14*(raids* raids))/4;
+  print('Circle Area Inscribed in a Square $area');
+}
