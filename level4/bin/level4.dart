e93c99811df5bbfad49c90ab2df9b1768f67ea95
engIbrahemQruea <mmany6186@gmail.com>
2026-02-04 23:43:04 +0300

    feat: init project Algorithms & Problem Solving – Level 4

---
+++ level4/bin/level4.dart
@@ -0,0 +1,5 @@
+import 'package:level4/level4.dart' as level4;
+
+void main(List<String> arguments) {
+  print('Hello Ibrahim Qurea: ${level4.calculate()}!');
+}
---
+++ level4/test/level4_test.dart
@@ -0,0 +1,8 @@
+import 'package:level4/level4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
