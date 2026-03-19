d1708664d65d444df25f29276e7f59449025443e
engIbrahemQruea <mmany6186@gmail.com>
2026-03-19 21:25:17 +0300

    Initial commit: Create Project Level5

---
+++ level5/bin/level5.dart
@@ -0,0 +1,5 @@
+import 'package:level5/level5.dart' as level5;
+
+void main(List<String> arguments) {
+  print('Hello world: ${level5.calculate()}!');
+}
---
+++ level5/test/level5_test.dart
@@ -0,0 +1,8 @@
+import 'package:level5/level5.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
