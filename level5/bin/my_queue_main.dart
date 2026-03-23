593809a4563c088fc6b28bf5c482bfc893b10f83
engIbrahemQruea <mmany6186@gmail.com>
2026-03-23 22:34:42 +0300

    feat: Solve Extensions 1 to 7 Requirements (...)

--- level5/bin/my_queue_main.dart
+++ level5/bin/my_queue_main.dart
@@ -1,3 +1,4 @@
+import 'package:level5/models/my_dbl_linked_list.dart';
 import 'package:level5/models/my_queue.dart';
 
 void main() {
@@ -17,9 +18,46 @@ void main() {
   print('Front Item: ${myQueue.front()}');
   print('Back Item: ${myQueue.back()}');
 
-myQueue.pop();
-myQueue.printQueue();
+  myQueue.pop();
+  print('After Pop:');
+  myQueue.printQueue();
 
+  // Extention #1
+  print('Item at index 2: ${myQueue.getItem(2)}');
+  print('');
+  // Extention #2
+  myQueue.reverse();
+  print('After Reverse:');
+  myQueue.printQueue();
+  print('');
 
-  
+  // Extention #3
+  myQueue.updateAt(2, 600);
+  print('After Update at index 2:');
+  myQueue.printQueue();
+  print('');
+
+  // Extention #4
+  myQueue.insertAfter(2, 800);
+  print('After Insertion 800 After 2:');
+  myQueue.printQueue();
+  print('');
+
+  // Extention #5
+  myQueue.insertAtFront(1000);
+  print('After Insertion 1000 At Front:');
+  myQueue.printQueue();
+  print('');
+
+  // Extention #6
+  myQueue.insertAtBack(2000);
+  print('After Insertion 2000 At Back:');
+  myQueue.printQueue();
+  print('');
+
+  // Extention #7
+  myQueue.clear();
+  print('After Clear:');
+  myQueue.printQueue();
+  print('');
 }
--- level5/lib/models/my_queue.dart
+++ level5/lib/models/my_queue.dart
@@ -27,7 +27,34 @@ class MyQueue<T> {
   T? back() =>
       isEmpty ? throw StateError('Queue is empty') : _myList.getItem(size - 1);
 
-  void clear() => _myList.clear();
-
   void printQueue() => _myList.printAll();
+
+  /// Extention #1
+  T? getItem(int index) => _myList.getItem(index);
+
+  /// Extention #2
+  void reverse() => _myList.reverse();
+
+  /// Extention #3
+  bool updateAt(int index, T value) => _myList.updateAt(index, value);
+
+  /// Extention #4
+  void insertAfter(int index, T value) {
+    final targetNode = _myList.getNodeAt(index);
+
+    if (targetNode != null) {
+      _myList.insertAfter(targetNode, value);
+    } else {
+      throw RangeError("Index $index out of bounds for size $size");
+    }
+  }
+
+  /// Extention #5
+  void insertAtFront(T value) => _myList.insertAtBeginning(value);
+
+  /// Extention #6
+  void insertAtBack(T value) => _myList.insertAtEnd(value);
+
+  /// Extention #7
+  void clear() => _myList.clear();
 }
