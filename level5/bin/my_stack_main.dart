cdbf7ecbcf54ed78695cb2f18d6dfcddc25906f0
engIbrahemQruea <mmany6186@gmail.com>
2026-03-23 23:32:31 +0300

    feat: Solve  Project 3 My Stack Requirements

--- level5/bin/my_stack_main.dart
+++ level5/bin/my_stack_main.dart
@@ -1,1 +1,51 @@
-void main() {}
+import 'package:level5/models/my_queue.dart';
+
+void main() {
+  final myStack = MyStack<int>();
+
+  myStack.push(10);
+  myStack.push(20);
+  myStack.push(30);
+  myStack.push(40);
+  myStack.push(50);
+
+  print('Stack Content:');
+  myStack.printStack();
+
+  print('Stack Size: ${myStack.size}');
+
+  print('Top Item: ${myStack.top()}');
+  print('Bottom Item: ${myStack.bottom()}');
+
+  myStack.pop();
+  print('After Pop:');
+  myStack.printStack();
+
+  // Extention #1
+  print('Item at index 2: ${myStack.getItem(2)}');
+  print('');
+
+  // Extention #2
+  myStack.reverse();
+  print('After Reverse:');
+  myStack.printStack();
+  print('');
+
+  // Extention #3
+  myStack.updateAt(2, 600);
+  print('After Update at index 2:');
+  myStack.printStack();
+  print('');
+
+  // Extention #4
+  myStack.insertAfter(2, 800);
+  print('After Insertion 800 After 2:');
+  myStack.printStack();
+  print('');
+
+  // Extention #5
+  myStack.clear();
+  print('After Clear:');
+  myStack.printStack();
+  print('');
+}
--- level5/lib/models/my_stack.dart
+++ level5/lib/models/my_stack.dart
@@ -1,1 +1,13 @@
+part of 'my_queue.dart';
 
+class MyStack<T> extends MyQueue<T> {
+  @override
+  void push(T value) => _myList.insertAtBeginning(value);
+
+  @override
+  T? top() => front();
+
+  T? bottom() => back();
+
+  void printStack() => printQueue();
+}
