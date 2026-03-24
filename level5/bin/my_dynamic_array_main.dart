bfeff09cfa5dc47775bf6e013efb7c57123d1d94
engIbrahemQruea <mmany6186@gmail.com>
2026-03-24 16:38:53 +0300

    feat: Solve Project4 My DynamicArray Requirements

--- level5/bin/my_dynamic_array_main.dart
+++ level5/bin/my_dynamic_array_main.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:level5/models/my_dynamic_array.dart';
+
+void main() {
+  MyDynamicArray<int> myDynamicArray = MyDynamicArray(5);
+
+  myDynamicArray.add(10);
+  myDynamicArray.add(20);
+
+  myDynamicArray.add(30);
+
+  myDynamicArray.add(40);
+  myDynamicArray.add(50);
+
+  myDynamicArray.setItem(2, 35);
+
+  print("Is Empty: ${myDynamicArray.isEmpty}");
+  print("Is Full: ${myDynamicArray.isFull}");
+  print(" list size: ${myDynamicArray.size}");
+  print(" list capacity: ${myDynamicArray.capacity}");
+  print('List Items:');
+  myDynamicArray.printList();
+}
--- level5/lib/models/my_dynamic_array.dart
+++ level5/lib/models/my_dynamic_array.dart
@@ -1,1 +1,66 @@
-class MyDynamicArray<T> {}
+import 'dart:io';
+
+class MyDynamicArray<T> {
+  int _size = 0;
+  int _capacity = 0;
+  late List<T?> _array;
+
+  MyDynamicArray([int initialCapacity = 10]) {
+    if (initialCapacity < 0) {
+      throw ArgumentError('Capacity cannot be negative');
+    }
+    _capacity = initialCapacity;
+    _array = List<T?>.filled(_capacity, null);
+  }
+
+  int get size => _size;
+
+  int get capacity => _capacity;
+
+  bool get isEmpty => _size == 0;
+
+  bool get isFull => _size == _capacity;
+
+  bool get isNotEmpty => !isEmpty;
+
+  void setItem(int index, T value) {
+    if (index < 0 || index >= _size) {
+      throw RangeError.index(
+        index,
+        _array,
+        'index',
+        'Index out of bounds',
+        _size,
+      );
+    }
+    _array[index] = value;
+  }
+
+  void add(T value) {
+    if (_size == _capacity) {
+      _resize(_capacity == 0 ? 1 : _capacity * 2);
+    }
+    _array[_size] = value;
+    _size++;
+  }
+
+  T getItem(int index) {
+    if (index < 0 || index >= _size) {
+      throw RangeError.index(
+        index,
+        _array,
+        'index',
+        'Index out of bounds',
+        _size,
+      );
+    }
+    return _array[index] as T;
+  }
+
+  void printList() {
+    for (int i = 0; i < _size; i++) {
+      stdout.write('${_array[i]} ');
+    }
+    stdout.writeln();
+  }
+}
