d97b55aebdb8017bd7d750c4220143585c486009
engIbrahemQruea <mmany6186@gmail.com>
2026-03-24 22:15:20 +0300

    feat: Solve Project 5 MyQueueArr Requirements

--- level5/bin/my_queue_arr_main.dart
+++ level5/bin/my_queue_arr_main.dart
@@ -1,3 +1,63 @@
+import 'package:level5/models/my_queue_arr.dart';
 
+void main() {
+  final myQueueArr = MyQueueArr<int>();
 
-void main(){}
+  myQueueArr.push(10);
+  myQueueArr.push(20);
+  myQueueArr.push(30);
+  myQueueArr.push(40);
+  myQueueArr.push(50);
+
+  print('Queue Content:');
+  myQueueArr.printQueue();
+
+  print('Queue Size: ${myQueueArr.size}');
+
+  print('Front Item: ${myQueueArr.front()}');
+  print('Back Item: ${myQueueArr.back()}');
+
+  myQueueArr.pop();
+  print('After Pop:');
+  myQueueArr.printQueue();
+
+  // Extention #1
+  print('Item at index 2: ${myQueueArr.getItem(2)}');
+  print('');
+
+  // Extention #2
+  myQueueArr.reverse();
+  print('After Reverse:');
+  myQueueArr.printQueue();
+  print('');
+
+  // Extention #3
+  myQueueArr.updateAt(2, 600);
+  print('After Update at index 2:');
+  myQueueArr.printQueue();
+  print('');
+
+  // Extention #4
+  myQueueArr.insertAfter(2, 800);
+  print('After Insertion 800 After 2:');
+  myQueueArr.printQueue();
+  print('');
+
+  // Extention #5
+  myQueueArr.insertAtFront(1000);
+  print('After Insertion 1000 At Front:');
+  myQueueArr.printQueue();
+  print('');
+
+  // Extention #6
+  myQueueArr.insertAtBack(2000);
+  print('After Insertion 2000 At Back:');
+  myQueueArr.printQueue();
+  print('');
+
+  // Extention #7
+  myQueueArr.clear();
+  print('After Clear:');
+  myQueueArr.printQueue();
+  print('');
+}
--- level5/lib/models/my_queue_arr.dart
+++ level5/lib/models/my_queue_arr.dart
@@ -1,1 +1,59 @@
-class MyQueueArr {}
+import 'package:level5/models/my_dynamic_array.dart';
+
+class MyQueueArr<T> {
+  final MyDynamicArray<T> _myList = MyDynamicArray<T>();
+
+  int get size => _myList.size;
+
+  bool get isEmpty => _myList.isEmpty;
+
+  bool get isNotEmpty => _myList.isNotEmpty;
+
+  void push(T value) => _myList.insertAtEnd(value);
+
+  T pop() {
+    if (isEmpty) {
+      throw StateError('Queue is empty');
+    }
+
+    final value = _myList.getItem(0)!;
+    _myList.removeFirst();
+    return value;
+  }
+
+  T? front() =>
+      isEmpty ? throw StateError('Queue is empty') : _myList.getItem(0);
+
+  T? back() => isEmpty
+      ? throw StateError('Queue is empty')
+      : _myList.getItem(_myList.size - 1);
+
+  void printQueue() => _myList.printList();
+
+  /// Extention #1
+  T? getItem(int index) => _myList.getItem(index);
+
+  /// Extention #2
+  void reverse() => _myList.reverse();
+
+  /// Extention #3
+  void updateAt(int index, T value) => _myList.setItem(index, value);
+
+  /// Extention #4
+  void insertAfter(int index, T value) {
+    if (index < 0 || index >= size) {
+      throw RangeError("Index $index out of bounds for size $size");
+    }
+
+    _myList.insertAfter(index + 1, value);
+  }
+
+  /// Extention #5
+  void insertAtFront(T value) => _myList.insertAtBeginning(value);
+
+  /// Extention #6
+  void insertAtBack(T value) => _myList.insertAtEnd(value);
+
+  /// Extention #7
+  void clear() => _myList.clear();
+}
