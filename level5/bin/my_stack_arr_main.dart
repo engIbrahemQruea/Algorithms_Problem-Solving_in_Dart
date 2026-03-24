85e98353d56f1916a40f98b42dbbdee138875073
engIbrahemQruea <mmany6186@gmail.com>
2026-03-24 22:51:37 +0300

    feat: Solve Project 6 MyStackArr  Requirements

--- level5/bin/my_stack_arr_main.dart
+++ level5/bin/my_stack_arr_main.dart
@@ -1,1 +1,51 @@
-void main() {}
+import 'package:level5/models/my_queue_arr.dart';
+
+void main() {
+  final myStackArr = MyStackArr<int>();
+
+  myStackArr.push(10);
+  myStackArr.push(20);
+  myStackArr.push(30);
+  myStackArr.push(40);
+  myStackArr.push(50);
+
+  print('Stack Content:');
+  myStackArr.printStack();
+
+  print('Stack Size: ${myStackArr.size}');
+
+  print('Top Item: ${myStackArr.top()}');
+  print('Bottom Item: ${myStackArr.bottom()}');
+
+  myStackArr.pop();
+  print('After Pop:');
+  myStackArr.printStack();
+
+  // Extention #1
+  print('Item at index 2: ${myStackArr.getItem(2)}');
+  print('');
+
+  // Extention #2
+  myStackArr.reverse();
+  print('After Reverse:');
+  myStackArr.printStack();
+  print('');
+
+  // Extention #3
+  myStackArr.updateAt(2, 600);
+  print('After Update at index 2:');
+  myStackArr.printStack();
+  print('');
+
+  // Extention #4
+  myStackArr.insertAfter(2, 800);
+  print('After Insertion 800 After 2:');
+  myStackArr.printStack();
+  print('');
+
+  // Extention #5
+  myStackArr.clear();
+  print('After Clear:');
+  myStackArr.printStack();
+  print('');
+}
--- level5/lib/models/my_stack_arr.dart
+++ level5/lib/models/my_stack_arr.dart
@@ -1,1 +1,29 @@
-class MyStackArr<T> {}
+part of 'my_queue_arr.dart';
+
+class MyStackArr<T> extends MyQueueArr<T> {
+  @override
+  void push(T value) => _myList.add(value);
+
+  @override
+  T pop() {
+    if (isEmpty) {
+      throw StateError('Stack is empty');
+    }
+
+    final value = _myList.getItem(size - 1);
+    _myList.removeLast();
+    return value;
+  }
+
+  T top() {
+    if (isEmpty) throw StateError('Stack is empty');
+    return _myList.getItem(size - 1);
+  }
+
+  T bottom() {
+    if (isEmpty) throw StateError('Stack is empty');
+    return _myList.getItem(0);
+  }
+
+  void printStack() => printQueue();
+}
