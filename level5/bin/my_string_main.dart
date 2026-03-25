652acc2401ec60e4dbc4cd54b610ee9f787ac02a
engIbrahemQruea <mmany6186@gmail.com>
2026-03-25 21:37:47 +0300

    feat: Solve Project 7 Requirements ( Undo  Redo )

--- level5/bin/my_string_main.dart
+++ level5/bin/my_string_main.dart
@@ -1,1 +1,30 @@
-void main() {}
+import 'package:level5/models/project7/my_string_undo_redo.dart';
+
+void main() {
+  final myString = MyStringUndoRedo<String>();
+
+  myString.setValue = "Ibrahim";
+  print(myString.getValue);
+  myString.setValue = 'Ibrahim1';
+  print(myString.getValue);
+  myString.setValue = 'Ibrahim2';
+  print(myString.getValue);
+  myString.setValue = 'Ibrahim3';
+  print(myString.getValue);
+
+  print("---------------Undo------------");
+  myString.undo();
+  print('After Undo : ${myString.getValue}');
+  myString.undo();
+  print('After Undo : ${myString.getValue}');
+  myString.undo();
+  print('After Undo : ${myString.getValue}');
+
+  print("---------------Redo------------");
+  myString.redo();
+  print('After Redo : ${myString.getValue}');
+  myString.redo();
+  print('After Redo : ${myString.getValue}');
+  myString.redo();
+  print('After Redo : ${myString.getValue}');
+}
--- level5/lib/models/project7/my_string_undo_redo.dart
+++ level5/lib/models/project7/my_string_undo_redo.dart
@@ -1,2 +1,34 @@
+import 'package:level5/models/my_queue_arr.dart';
 
-class MyStringUndoRedo<T> {}
+class MyStringUndoRedo<T> {
+  final MyStackArr<T> _undoStack = MyStackArr<T>();
+  final MyStackArr<T> _redoStack = MyStackArr<T>();
+
+  T? _currentValue;
+
+  T? get getValue => _currentValue;
+
+  set setValue(T newValue) {
+    if (_currentValue != null) {
+      _undoStack.push(_currentValue!);
+    }
+    _currentValue = newValue;
+    _redoStack.clear();
+  }
+
+  void undo() {
+    if (_undoStack.isEmpty) return;
+    if (_currentValue != null) {
+      _redoStack.push(_currentValue!);
+    }
+    _currentValue = _undoStack.pop();
+  }
+
+  void redo() {
+    if (_redoStack.isEmpty) return;
+    if (_currentValue != null) {
+      _undoStack.push(_currentValue!);
+    }
+    _currentValue = _redoStack.pop();
+  }
+}
